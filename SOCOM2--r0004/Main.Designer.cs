4684b533d588b8459e9695fe5e753ad64c1a957a
xCENTx <80198020+xCENTx@users.noreply.github.com>
2021-08-23 09:37:33 -0400

    INITIAL UPLOAD
    - PERFECT SHOT

---
+++ SOCOM2--r0004/Main.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace NightFyre_SOCOM_2
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ScanButton = new System.Windows.Forms.Button();
+            this.PerfectShotCheckBox = new System.Windows.Forms.CheckBox();
+            this.debugLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.debugAddr3_label = new System.Windows.Forms.Label();
+            this.debugAddr2_label = new System.Windows.Forms.Label();
+            this.debugAddr1_label = new System.Windows.Forms.Label();
+            this.PatchButton = new System.Windows.Forms.Button();
+            this.ProcessTimer = new System.Windows.Forms.Timer(this.components);
+            this.MemoryTimer = new System.Windows.Forms.Timer(this.components);
+            this.debugAddr4_label = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ScanButton
+            // 
+            this.ScanButton.Font = new System.Drawing.Font("Verdana", 10F);
+            this.ScanButton.Location = new System.Drawing.Point(7, 12);
+            this.ScanButton.Name = "ScanButton";
+            this.ScanButton.Size = new System.Drawing.Size(60, 23);
+            this.ScanButton.TabIndex = 0;
+            this.ScanButton.Text = "SCAN";
+            this.ScanButton.UseVisualStyleBackColor = true;
+            this.ScanButton.Click += new System.EventHandler(this.ScanButton_Click);
+            // 
+            // PerfectShotCheckBox
+            // 
+            this.PerfectShotCheckBox.AutoSize = true;
+            this.PerfectShotCheckBox.Font = new System.Drawing.Font("Verdana", 10F);
+            this.PerfectShotCheckBox.Location = new System.Drawing.Point(73, 14);
+            this.PerfectShotCheckBox.Name = "PerfectShotCheckBox";
+            this.PerfectShotCheckBox.Size = new System.Drawing.Size(135, 21);
+            this.PerfectShotCheckBox.TabIndex = 1;
+            this.PerfectShotCheckBox.Text = "PERFECT SHOT";
+            this.PerfectShotCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // debugLabel
+            // 
+            this.debugLabel.AutoSize = true;
+            this.debugLabel.Font = new System.Drawing.Font("Verdana", 10F);
+            this.debugLabel.Location = new System.Drawing.Point(3, 3);
+            this.debugLabel.Name = "debugLabel";
+            this.debugLabel.Size = new System.Drawing.Size(41, 17);
+            this.debugLabel.TabIndex = 2;
+            this.debugLabel.Text = "stuff";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.debugLabel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 122);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(204, 25);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.debugAddr4_label);
+            this.panel2.Controls.Add(this.debugAddr3_label);
+            this.panel2.Controls.Add(this.debugAddr2_label);
+            this.panel2.Controls.Add(this.debugAddr1_label);
+            this.panel2.Location = new System.Drawing.Point(100, 41);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(92, 77);
+            this.panel2.TabIndex = 4;
+            // 
+            // debugAddr3_label
+            // 
+            this.debugAddr3_label.AutoSize = true;
+            this.debugAddr3_label.Font = new System.Drawing.Font("Verdana", 10F);
+            this.debugAddr3_label.Location = new System.Drawing.Point(3, 36);
+            this.debugAddr3_label.Name = "debugAddr3_label";
+            this.debugAddr3_label.Size = new System.Drawing.Size(48, 17);
+            this.debugAddr3_label.TabIndex = 4;
+            this.debugAddr3_label.Text = "label2";
+            // 
+            // debugAddr2_label
+            // 
+            this.debugAddr2_label.AutoSize = true;
+            this.debugAddr2_label.Font = new System.Drawing.Font("Verdana", 10F);
+            this.debugAddr2_label.Location = new System.Drawing.Point(3, 19);
+            this.debugAddr2_label.Name = "debugAddr2_label";
+            this.debugAddr2_label.Size = new System.Drawing.Size(48, 17);
+            this.debugAddr2_label.TabIndex = 3;
+            this.debugAddr2_label.Text = "label1";
+            // 
+            // debugAddr1_label
+            // 
+            this.debugAddr1_label.AutoSize = true;
+            this.debugAddr1_label.Font = new System.Drawing.Font("Verdana", 10F);
+            this.debugAddr1_label.Location = new System.Drawing.Point(3, 2);
+            this.debugAddr1_label.Name = "debugAddr1_label";
+            this.debugAddr1_label.Size = new System.Drawing.Size(48, 17);
+            this.debugAddr1_label.TabIndex = 2;
+            this.debugAddr1_label.Text = "label1";
+            // 
+            // PatchButton
+            // 
+            this.PatchButton.Font = new System.Drawing.Font("Verdana", 10F);
+            this.PatchButton.Location = new System.Drawing.Point(23, 95);
+            this.PatchButton.Name = "PatchButton";
+            this.PatchButton.Size = new System.Drawing.Size(71, 23);
+            this.PatchButton.TabIndex = 5;
+            this.PatchButton.Text = "PATCH";
+            this.PatchButton.UseVisualStyleBackColor = true;
+            this.PatchButton.Click += new System.EventHandler(this.PatchButton_Click);
+            // 
+            // ProcessTimer
+            // 
+            this.ProcessTimer.Enabled = true;
+            this.ProcessTimer.Tick += new System.EventHandler(this.ProcessTimer_Tick);
+            // 
+            // MemoryTimer
+            // 
+            this.MemoryTimer.Enabled = true;
+            this.MemoryTimer.Tick += new System.EventHandler(this.MemoryTimer_Tick);
+            // 
+            // debugAddr4_label
+            // 
+            this.debugAddr4_label.AutoSize = true;
+            this.debugAddr4_label.Font = new System.Drawing.Font("Verdana", 10F);
+            this.debugAddr4_label.Location = new System.Drawing.Point(3, 54);
+            this.debugAddr4_label.Name = "debugAddr4_label";
+            this.debugAddr4_label.Size = new System.Drawing.Size(48, 17);
+            this.debugAddr4_label.TabIndex = 5;
+            this.debugAddr4_label.Text = "label2";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(204, 147);
+            this.Controls.Add(this.PatchButton);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.PerfectShotCheckBox);
+            this.Controls.Add(this.ScanButton);
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.ShowIcon = false;
+            this.Text = "VIP";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainClosing);
+            this.Load += new System.EventHandler(this.MainLoad);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ScanButton;
+        private System.Windows.Forms.CheckBox PerfectShotCheckBox;
+        private System.Windows.Forms.Label debugLabel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label debugAddr3_label;
+        private System.Windows.Forms.Label debugAddr2_label;
+        private System.Windows.Forms.Label debugAddr1_label;
+        private System.Windows.Forms.Button PatchButton;
+        private System.Windows.Forms.Timer ProcessTimer;
+        private System.Windows.Forms.Timer MemoryTimer;
+        private System.Windows.Forms.Label debugAddr4_label;
+    }
+}
+
---
+++ SOCOM2--r0004/Main.cs
@@ -0,0 +1,409 @@
+﻿using Binarysharp.MemoryManagement;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Linq;
+using System;
+using Memory;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace NightFyre_SOCOM_2
+{
+    public partial class Main : Form
+    {
+        Mem _mem = new Mem();
+        public static string PROCESSNAME = "pcsx2";
+        private static MemorySharp m = new MemorySharp(Process.GetProcessesByName(PROCESSNAME).FirstOrDefault());
+        Dictionary<string, Binarysharp.MemoryManagement.Memory.RemoteAllocation> CodeCave = new Dictionary<string, Binarysharp.MemoryManagement.Memory.RemoteAllocation>();
+        Dictionary<string, IntPtr> ScanResult = new Dictionary<string, IntPtr>();
+        Dictionary<string, int> oldData = new Dictionary<string, int>();
+        private static IntPtr PLAYEROBJECT = new IntPtr(0x2044D648);
+        bool pcsx2Running;
+        bool HACKACTIVE = false;
+        bool PatternFound = false;
+        bool scan1FAIL = false;
+        bool scan2FAIL = false;
+        bool scan3FAIL = false;
+        bool scan4FAIL = false;
+
+        #region MAIN
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void MainLoad(object sender, EventArgs e)
+        {
+            debugLabel.Text = "";
+            debugAddr1_label.Text = "";
+            debugAddr2_label.Text = "";
+            debugAddr3_label.Text = "";
+            debugAddr4_label.Text = "";
+        }
+
+        private void MainClosing(object sender, FormClosingEventArgs e)
+        {
+            QUIT();
+        }
+
+        private void ProcessTimer_Tick(object sender, EventArgs e)
+        {
+            Process[] pcsx2 = Process.GetProcessesByName(PROCESSNAME);
+            if (pcsx2.Length > 0)
+            {
+                pcsx2Running = true;
+                return;
+            }
+            pcsx2Running = false;
+        }
+
+        private void MemoryTimer_Tick(object sender, EventArgs e)
+        {
+            if (!pcsx2Running)
+            {
+                return;
+            }
+
+            HACKMAIN();
+        }
+
+        private void ScanButton_Click(object sender, EventArgs e)
+        {
+            if (pcsx2Running) 
+            { 
+                if (!PerfectShotCheckBox.Checked) 
+                { 
+                    if (!HACKACTIVE)
+                    {
+                        debugLabel.ForeColor = Color.FromArgb(169, 169, 0);
+                        debugLabel.Text = "SCANNING .";
+                        SCANNER(); 
+                    } 
+                }
+                else
+                {
+                    debugLabel.ForeColor = Color.FromArgb(169, 0, 0);
+                    debugLabel.Text = "Is Checkbox Checked?";
+                }
+            } 
+            else 
+            { 
+                debugLabel.ForeColor = Color.FromArgb(169, 0, 0);  
+                debugLabel.Text = "PCSX2 NOT DETECTED";  
+            }
+        }
+
+        private void PatchButton_Click(object sender, EventArgs e)
+        {
+            if (HACKACTIVE)
+            {
+
+            }
+        }
+
+        private void SCANNER()
+        {
+            if (!_mem.OpenProcess(PROCESSNAME))
+            {
+                int PID = _mem.GetProcIdFromName(PROCESSNAME);
+                if (PID > 0)
+                {
+                    _mem.OpenProcess(PID);
+                    return;
+                }
+            }
+
+            if (!PatternFound)
+            {
+                R1SCAN();
+            }
+
+        }
+
+        private void HACKMAIN()
+        {
+            if (pcsx2Running)
+            {
+                if (PerfectShotCheckBox.Checked)
+                {
+                    if (!m.Read<byte>(PLAYEROBJECT, 4, false).SequenceEqual(new byte[] { 0, 0, 0, 0 }))
+                    {
+                        if (PatternFound)
+                        {
+                            if (!HACKACTIVE)
+                            {
+                                NO_RECOIL1();
+                                
+                                //So we dont repeat the write
+                                HACKACTIVE = true;
+                                debugLabel.Text = "SUCCESS";
+                            }
+                        }
+                        else
+                        {
+                            debugLabel.Text = "SCAN REQUIRED";
+                            debugLabel.ForeColor = Color.FromArgb(169, 0, 0);
+                        }
+                    }
+                    else if (HACKACTIVE)
+                    {
+                        debugLabel.Text = "NOT IN GAME";
+                        debugLabel.ForeColor = Color.FromArgb(169, 0, 0);
+                        QUIT();
+                    }
+                }
+                else if (HACKACTIVE)
+                {
+                    debugLabel.Text = "";
+                    QUIT();
+                }
+            }
+            else
+            {
+
+            }
+        }
+        #endregion
+        
+        #region SCANS
+        private async void R1SCAN()
+        {
+            //AoB Scan & Store Result
+            IEnumerable<long> PatternScan = await _mem.AoBScan("89 11 C7 05 B8 A1 ?? 01 40 AA 5C 00 A1 D0 A2 ?? 01", false, true);   //Pattern to search for
+            long PatternScanResult = PatternScan.FirstOrDefault();     //Store the result
+            if (PatternScanResult == 0)   //If the result is 0 , this means the player did not shoot
+            {
+                scan1FAIL = true;
+                FAIL();
+                return;     //We dont want to continue , try again
+            }
+            string test = PatternScanResult.ToString("X");    //We want our Address to be Hex
+            IntPtr HEXADDR = new IntPtr(Convert.ToInt32(test, 16));     //We also need it to be an IntPtr
+            ScanResult.Add("a", HEXADDR);   //Now we add it to a dictionary
+            debugLabel.Text = "SCANNING . .";
+            debugAddr1_label.Text = ScanResult["a"].ToString("X");
+            R2SCAN();
+        }
+
+        private async void R2SCAN()
+        {
+            //AoB Scan & Store Result
+            IEnumerable<long> PatternScan = await _mem.AoBScan("89 11 C7 05 B8 A1 ?? 01 40 AA 5C 00 83 05 D0 A2 ?? 01 04 E9", false, true);   //Pattern to search for
+            long PatternScanResult = PatternScan.FirstOrDefault();     //Store the result
+            if (PatternScanResult == 0)   //If the result is 0 , this means the player did not shoot
+            {
+                scan2FAIL = true;
+                FAIL();
+                return;     //We dont want to continue , try again
+            }
+            string test = PatternScanResult.ToString("X");    //We want our Address to be Hex
+            IntPtr HEXADDR = new IntPtr(Convert.ToInt32(test, 16));     //We also need it to be an IntPtr
+            ScanResult.Add("b", HEXADDR);   //Now we add it to a dictionary
+            debugLabel.Text = "SCANNING . . .";
+            debugAddr2_label.Text = ScanResult["b"].ToString("X");
+            S1SCAN();
+        }
+
+        private async void S1SCAN()
+        {
+            //AoB Scan & Store Result
+            IEnumerable<long> PatternScan = await _mem.AoBScan("89 11 C7 05 B8 A1 ?? 01 A8 AB 5C 00 A1 D0 A2 ?? 01 83 C0 0F", false, true);   //Pattern to search for
+            long PatternScanResult = PatternScan.FirstOrDefault();     //Store the result
+            if (PatternScanResult == 0)   //If the result is 0 , this means the player did not shoot
+            {
+                scan3FAIL = true;
+                FAIL();
+                return;     //We dont want to continue , try again
+            }
+            string test = PatternScanResult.ToString("X");    //We want our Address to be Hex
+            IntPtr HEXADDR = new IntPtr(Convert.ToInt32(test, 16));     //We also need it to be an IntPtr
+            ScanResult.Add("c", HEXADDR);   //Now we add it to a dictionary
+            debugLabel.Text = "SCANNING . . . .";
+            debugAddr3_label.Text = ScanResult["c"].ToString("X");
+            S2SCAN();
+        }
+
+        private async void S2SCAN()
+        {
+            //AoB Scan & Store Result
+            IEnumerable<long> PatternScan = await _mem.AoBScan("0F 88 ?? ?? ?? ?? 89 11 8B 0D 30 A0 ?? 01 81 C1 6C 08 00 00 89 C8 C1 E8 0C 8B 04 85 30 ?? ?? ?? BB ?? ?? ?? 30 01 C1 0F 88 ?? ?? ?? D? 8B 01 A3 04 9E EE 01", false, true);   //Pattern to search for
+            long PatternScanResult = PatternScan.FirstOrDefault();     //Store the result
+            if (PatternScanResult == 0)   //If the result is 0 , this means the player did not shoot
+            {
+                scan4FAIL = true;
+                FAIL();
+                return;     //We dont want to continue , try again
+            }
+            int adjust = (int)PatternScanResult + 0x06;
+            string test = adjust.ToString("X");    //We want our Address to be Hex
+            IntPtr HEXADDR = new IntPtr(Convert.ToInt32(test, 16));     //We also need it to be an IntPtr
+            ScanResult.Add("d", HEXADDR);   //Now we add it to a dictionary
+            debugLabel.Text = "SCANNING . . . .";
+            debugAddr4_label.Text = ScanResult["d"].ToString("X");
+
+            //Done with scans
+            debugLabel.Text = "READY";
+            debugLabel.ForeColor = Color.FromArgb(0, 169, 0);
+            PatternFound = true;    //We are done here
+        }
+
+        private async void RFSCAN()
+        {
+            //AoB Scan & Store Result
+            IEnumerable<long> PatternScan = await _mem.AoBScan("89 11 C7 05 B8 A1 ?? 01 40 AA 5C 00 A1 D0 A2 ?? 01", false, true);   //Pattern to search for
+            long PatternScanResult = PatternScan.FirstOrDefault();     //Store the result
+            if (PatternScanResult == 0)   //If the result is 0 , this means the player did not shoot
+            {
+                //scan5FAIL = true;
+                FAIL();
+                return;     //We dont want to continue , try again
+            }
+            string test = PatternScanResult.ToString("X");    //We want our Address to be Hex
+            IntPtr HEXADDR = new IntPtr(Convert.ToInt32(test, 16));     //We also need it to be an IntPtr
+            ScanResult.Add("e", HEXADDR);   //Now we add it to a dictionary
+            debugLabel.Text = "SCANNING . . . .";
+            debugAddr1_label.Text = ScanResult["e"].ToString("X");
+        }
+        #endregion
+
+        #region HACKS
+        private void NO_RECOIL1()
+        {
+            int ADDRESS = m.Read<int>(ScanResult["a"], false);      //Sometimes it wont be able to read
+            oldData.Add("RECOIL1", ADDRESS);
+            m.Assembly.Inject(
+                    new[]
+                    {
+                    "nop",
+                    "nop",
+                    },
+                    ScanResult["a"]);
+            NO_RECOIL2();
+        }
+
+        private void NO_RECOIL2()
+        {
+            int ADDRESS = m.Read<int>(ScanResult["b"], false);      //Sometimes it wont be able to read
+            oldData.Add("RECOIL2", ADDRESS);
+            m.Assembly.Inject(
+                    new[]
+                    {
+                    "nop",
+                    "nop",
+                    },
+                    ScanResult["b"]);
+            NO_SPREAD1();
+        }
+
+        private void NO_SPREAD1()
+        {
+            int ADDRESS = m.Read<int>(ScanResult["c"], false);      //Sometimes it wont be able to read
+            oldData.Add("SPREAD1", ADDRESS);
+            m.Assembly.Inject(
+                    new[]
+                    {
+                    "nop",
+                    "nop",
+                    },
+                    ScanResult["c"]);
+            NO_SPREAD2();
+        }
+
+        private void NO_SPREAD2()
+        {
+            int ADDRESS = m.Read<int>(ScanResult["d"], false);      //Sometimes it wont be able to read
+            oldData.Add("SPREAD2", ADDRESS);
+            m.Assembly.Inject(
+                    new[]
+                    {
+                    "nop",
+                    "nop",
+                    },
+                    ScanResult["d"]);
+        }
+
+        private void RAPID_FIRE()
+        {
+            int ADDRESS = m.Read<int>(ScanResult["e"], false);      //Sometimes it wont be able to read
+            oldData.Add("RAPIDFIRE", ADDRESS);
+            m.Assembly.Inject(
+                    new[]
+                    {
+                    "nop",
+                    "nop",
+                    },
+                    ScanResult["e"]);
+        }
+        #endregion
+
+        #region CLEAN UP
+        private void FAIL()
+        {
+            PatternFound = false;
+            debugLabel.ForeColor = Color.FromArgb(169, 0, 0);
+            discardDATA();
+        }
+
+        private void discardDATA()
+        {
+            if (scan1FAIL)
+            {
+                debugAddr1_label.Text = "";
+                debugLabel.Text = "ERROR: 1";
+                scan1FAIL = false;
+            }
+            if (scan2FAIL)
+            {
+                ScanResult.Remove("a");
+                debugAddr2_label.Text = "";
+                debugLabel.Text = "ERROR: 2";
+                scan2FAIL = false;
+            }
+            if (scan3FAIL)
+            {
+                ScanResult.Remove("b");
+                debugAddr3_label.Text = "";
+                debugLabel.Text = "ERROR: 3";
+                scan3FAIL = false;
+            }
+            if (scan4FAIL)
+            {
+                ScanResult.Remove("c");
+                debugAddr4_label.Text = "";
+                debugLabel.Text = "ERROR: 4";
+                scan3FAIL = false;
+            }
+        }
+
+        private void QUIT()
+        {
+            if (HACKACTIVE)
+            {
+                m.Write<int>(ScanResult["a"], oldData["RECOIL1"], false);
+                m.Write<int>(ScanResult["b"], oldData["RECOIL2"], false);
+                m.Write<int>(ScanResult["c"], oldData["SPREAD1"], false);
+                m.Write<int>(ScanResult["d"], oldData["SPREAD2"], false);
+                //m.Write<int>(ScanResult["e"], oldData["RAPIDFIRE"], false);
+                ScanResult.Remove("a");
+                ScanResult.Remove("b");
+                ScanResult.Remove("c");
+                ScanResult.Remove("d");
+                //ScanResult.Remove("e");
+                oldData.Remove("RECOIL1");
+                oldData.Remove("RECOIL2");
+                oldData.Remove("SPREAD1");
+                oldData.Remove("SPREAD2");
+                //oldData.Remove("RAPIDFIRE");
+                debugLabel.Text = "RE-SCAN REQUIRED";
+                debugLabel.ForeColor = Color.FromArgb(169, 0, 0);
+                debugAddr1_label.Text = "";
+                debugAddr2_label.Text = "";
+                debugAddr3_label.Text = "";
+                debugAddr4_label.Text = "";
+                HACKACTIVE = false;
+                PatternFound = false;
+            }
+        }
+        #endregion
+    }
+}
